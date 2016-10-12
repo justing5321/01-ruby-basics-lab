41ee79048882e5e7dfac897607630e7a784af95e
justing5321 <justing5321@hstat.org>
2016-10-12 22:57:33 +0000

    tried the homework

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
-def division
-    
+def division(num1,num2)
+    num1/num2
 end
 
 def argue
   
 end
 
-def greeting
+def greeting(pie,fish)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party()
+    "cheese"
 end
 
 def whisper
